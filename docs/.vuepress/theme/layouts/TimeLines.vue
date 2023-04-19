4c5a02f31a4fd467996ef47e828de99557741b2c
liuwenbo <liuwb-work@outlook.com>
2023-04-19 14:58:30 +0800

    更新样式

--- docs/.vuepress/theme/layouts/TimeLines.vue
+++ docs/.vuepress/theme/layouts/TimeLines.vue
@@ -70,35 +70,35 @@ export default defineComponent({
     box-sizing border-box
     position relative
     list-style none
-    &::after {
-      content: " ";
-      position: absolute;
-      top: 14px;
-      left: 0;
-      z-index: -1;
-      margin-left: -2px;
-      width: 4px;
-      height: 100%;
-      background: var(--border-color);
-    }
+    //&::after {
+    //  content: " ";
+    //  position: absolute;
+    //  top: 14px;
+    //  left: 0;
+    //  z-index: -1;
+    //  margin-left: -2px;
+    //  width: 4px;
+    //  height: 100%;
+    //  background: var(--border-color);
+    //}
     .desc, .year {
       position: relative;
       color var(--text-color);
       font-size 16px
-      &:before {
-        content: " ";
-        position: absolute;
-        z-index 2;
-        left: -20px;
-        top: 50%;
-        margin-left: -4px;
-        margin-top: -4px;
-        width: 8px;
-        height: 8px;
-        background: var(--background-color);
-        border: 1px solid var(--border-color);
-        border-radius: 50%;
-      }
+      //&:before {
+      //  content: " ";
+      //  position: absolute;
+      //  z-index 2;
+      //  left: -20px;
+      //  top: 50%;
+      //  margin-left: -4px;
+      //  margin-top: -4px;
+      //  width: 8px;
+      //  height: 8px;
+      //  background: var(--background-color);
+      //  border: 1px solid var(--border-color);
+      //  border-radius: 50%;
+      //}
     }
     .year {
       margin: 80px 0 0px;
@@ -110,7 +110,6 @@ export default defineComponent({
       padding-left 0!important
       li {
         display flex
-        padding 30px 0 10px
         list-style none
         border-bottom: 1px dashed var(--border-color);
         position relative
@@ -122,7 +121,7 @@ export default defineComponent({
             }
           }
           .title {
-            color $accentColor
+            color $accentColor !important
           }
         }
         .date {
@@ -130,23 +129,23 @@ export default defineComponent({
           line-height 30px
           color var(--text-color-sub)
           font-size 12px
-          &::before {
-            content: " ";
-            position: absolute;
-            left: -18px;
-            top: 41px;
-            width: 6px;
-            height: 6px;
-            margin-left: -4px;
-            background: var(--background-color);
-            border-radius: 50%;
-            border: 1px solid var(--border-color);
-            z-index 2
-          }
+          //&::before {
+          //  content: " ";
+          //  position: absolute;
+          //  left: -18px;
+          //  top: 41px;
+          //  width: 6px;
+          //  height: 6px;
+          //  margin-left: -4px;
+          //  background: var(--background-color);
+          //  border-radius: 50%;
+          //  border: 1px solid var(--border-color);
+          //  z-index 2
+          //}
         }
         .title {
           line-height 30px
-          color var(--text-color-sub)
+          color #394E6A !important
           font-size 16px
           cursor pointer
         }
